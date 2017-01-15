d141160c023f9ab90e9edd3b0dd37c9e19bb1387
olivecoil <adrian.s.tran@gmail.com>
2017-01-14 19:56:37 -0800

    Added messageList component to render the chatmessages from the server

---
+++ app/messageList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+} from 'react-native';
+
+class MessageList extends Component {
+  render(){
+    var list = this.props.messages.map(function(message){
+      return (
+        <View>
+          <Text>{message.username} - {message.time}</Text>
+          <Text>{message.text}</Text>
+        </View>
+      )
+    });
+      return (
+          {list}
+      );
+  }
+}
